afeec9a8b7356493a1a72bde164c8c6a26bc51f6
Moonismona <ichehahajar@gmail.com>
2023-02-23 13:33:39 -0800

    More functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,14 @@
 
 void print_square(int size)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
+		for (j = 0; j < size; j++)
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
 }
