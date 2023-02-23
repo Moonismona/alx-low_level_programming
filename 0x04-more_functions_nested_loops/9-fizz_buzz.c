77aae1f32a63031f00c4bd47eb3ab018e806d36b
Moonismona <ichehahajar@gmail.com>
2023-02-23 15:24:04 -0800

    Final

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,23 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0)
+	for (i = 1; i < 100; i++)
+		if (i % 15 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 15 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-	}
 	printf("Buzz");
 	printf("\n");
 	return (0);
