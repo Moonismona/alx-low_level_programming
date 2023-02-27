ce03ba1711b6259f1aa504209fccae17289d3156
Moonismona <ichehahajar@gmail.com>
2023-02-27 04:23:37 -0800

    Adding 0-reset_to_98.c file. It works!

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as and updates the value to 98.
+ * @n: pointer.
+ * Return: no value.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
