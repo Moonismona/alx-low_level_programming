09b6187a936eb98c2539f50f0db87c864a7929bf
Moonismona <ichehahajar@gmail.com>
2023-02-27 13:16:46 -0800

    Adding 5-rev_string.c file. It works!

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: string.
+ * Return: no return.
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (int i = 0; i < len / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
